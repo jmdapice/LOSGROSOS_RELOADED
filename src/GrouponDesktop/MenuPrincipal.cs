896576be884408637da095ee49716599318d4189
jmdapice <pepe@perez.com>
2012-11-10 21:02:57 -0300

    juanca

--- src/GrouponDesktop/MenuPrincipal.cs
+++ src/GrouponDesktop/MenuPrincipal.cs
@@ -235,6 +235,7 @@ namespace GrouponDesktop
 
             private void cerrarSesionToolStripMenuItem_Click(object sender, EventArgs e)
             {
+                //juanca
             }
            
 
